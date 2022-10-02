a9829e6580e77f3ff29a28fc1a2e765043342bd9
maximan81 <ericmaximan@gmail.com>
2022-10-02 18:55:11 +0200

    Static code analysis

--- src/main/java/com/leszko/calculator/CalculatorApplication.java
+++ src/main/java/com/leszko/calculator/CalculatorApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalculatorApplication {
+	
+	private static final String CONSTANT = "constant";
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
