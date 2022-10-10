92bee8223ecd3488ca54c592ac2ac0d12344f119
maximan81 <ericmaximan@gmail.com>
2022-10-10 10:59:04 +0200

    Deployment steps update

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-test $(curl localhost:8765/sum?a=1\&b=2) -eq 3
+#test $(curl localhost:8765/sum?a=1\&b=2) -eq 3
+
+set -x
+
+NODE_IP=$(kubectl get nodes -o jsonpath='{ $.items[0].status.addresses[?(@.type=="ExternalIP")].address }')
+NODE_PORT=$(kubectl get svc calculator-service -o=jsonpath='{.spec.ports[0].nodePort}')
+./gradlew acceptanceTest -Dcalculator.url=http://${NODE_IP}:${NODE_PORT}
+
